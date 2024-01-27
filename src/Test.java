d794f53e5feffd9bec5bf49c9b8a95d0d85d1f51
Prexashah <146986871+Prexashah@users.noreply.github.com>
2024-01-27 15:09:46 +0000

    Commiting changes

--- src/Test.java
+++ src/Test.java
@@ -1,6 +1,6 @@
 public class Test {
     public static void main(String[] args) {
-        System.out.println("My name is Prexa");
+        System.out.println("My name is Prexa, How are you?");
         System.out.println("Hello");
     }
 
